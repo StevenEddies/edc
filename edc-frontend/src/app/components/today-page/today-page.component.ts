a26eb0fddf80a8332773c7e9201d1c66d25f027e
Steven Eddies <steven@eddies.me.uk>
2022-01-02 11:43:38 +0000

    Tidied up today page styling, including correct date.

--- edc-frontend/src/app/components/today-page/today-page.component.ts
+++ edc-frontend/src/app/components/today-page/today-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-today-page',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./today-page.component.css']
 })
 export class TodayPageComponent implements OnInit {
+
+  today = new Date();
 
   constructor() { }
 
