be0961a4fa82df050194a50885e7428e435101c4
Steven Eddies <steven@eddies.me.uk>
2022-02-10 20:29:10 +0000

    Added authentication guard and placeholder service.

---
+++ edc-frontend/src/app/services/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { User } from '../../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private user: BehaviorSubject<User | null>;
+  
+  constructor(private router: Router) {
+    var storedJson = localStorage.getItem('user');
+    var storedUser = storedJson ? JSON.parse(storedJson) : null;
+    this.user = new BehaviorSubject<User | null>(storedUser);
+  }
+  
+  getUser(): Observable<User | null> {
+    return this.user.asObservable();
+  }
+  
+  getUserValue(): User | null {
+    return this.user.getValue();
+  }
+  
+  login(user: User): void {
+    this.user.next(user);
+    localStorage.setItem('user', JSON.stringify(this.user));
+    this.router.navigate(['/today']);
+  }
+  
+  logout(): void {
+    this.user.next(null);
+    localStorage.removeItem('user');
+    this.router.navigate(['/login']);
+  }
+}
